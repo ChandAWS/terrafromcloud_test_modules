11b49b2a829028c13c026c2664753441ead77155
chand <chand.nalubolu@gmail.com>
2021-06-06 20:27:21 +0530

    Core files

--- efs.tf
+++ efs.tf
@@ -1,7 +1,7 @@
-# module devopsb16test_efs {
-#   source = "app.terraform.io/ChandDEVOPS/efs/aws"
-#   version = "1.0.0"
-#   efsname  = "DevOpsB17EFS"
-#   subnets = "${module.devopsb16-testvpc.all_public_subnets}"
-#   sg = "${module.devopsb16-testvpc.sgid}"
-# }
+module devopsb16test_efs {
+  source = "app.terraform.io/ChandDEVOPS/efs/aws"
+  version = "1.0.0"
+  efsname  = "DevOpsB17EFS"
+  subnets = "${module.devopsb16-testvpc.all_public_subnets}"
+  sg = "${module.devopsb16-testvpc.sgid}"
+}
--- iam.tf
+++ iam.tf
@@ -1,16 +1,16 @@
 #Creating Role with name DevOpsB16TestRole
-# module devopsb16test_iam {
-#   source = "app.terraform.io/ChandDEVOPS/iam/aws"
-#   version = "1.0.0"
-#   rolename  = "DevOpsB16TestRole"
-#   envname = "${module.devopsb16-testvpc.env}"
-#   instanceprofilename = "devopsb16test-ins-profile"
-# }
+module devopsb16test_iam {
+  source = "app.terraform.io/ChandDEVOPS/iam/aws"
+  version = "1.0.0"
+  rolename  = "DevOpsB16TestRole"
+  envname = "${module.devopsb16-testvpc.env}"
+  instanceprofilename = "devopsb16test-ins-profile"
+}
 
-# #Creating IAM Policy and assigning to DevOpsB16TestRole
-# module devopsb16_iam_policy {
-#   source = "app.terraform.io/ChandDEVOPS/iam-attachment/aws"
-#   version = "1.0.0"
-#   attachroles = "${module.devopsb16test_iam.rolename}"
-#   policyname = "devopsb16-iam-policy1"
-# }
+#Creating IAM Policy and assigning to DevOpsB16TestRole
+module devopsb16_iam_policy {
+  source = "app.terraform.io/ChandDEVOPS/iam-attachment/aws"
+  version = "1.0.0"
+  attachroles = "${module.devopsb16test_iam.rolename}"
+  policyname = "devopsb16-iam-policy1"
+}
